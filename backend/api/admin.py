000e213efb6014c3c4b094ecc6c3185d576bcde3
Oleksandr Shyshatskyi <oshyshatskyi@cloudlinux.com>
2021-03-02 19:29:32 +0200

    added simple issue model and fsm implementation

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Issue
+from django_fsm_log.admin import StateLogInline
+from fsm_admin.mixins import FSMTransitionMixin
+
+
+class IssueModelAdmin(FSMTransitionMixin, admin.ModelAdmin):
+    # the name of one or more FSMFields on the model to transition
+    fsm_field = ['state', ]
+    # for pretty view of transitions list
+    inlines = [StateLogInline]
+
+
+admin.site.register(Issue, IssueModelAdmin)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django_fsm import FSMIntegerField, transition
 
-# Create your models here.
+
+class Issue(models.Model):
+    """
+    Describes single problem.
+
+    Single issue may contain multiple photos in some range
+    (e.g. when there are many potholes on some location)
+    """
+    STATUS_DRAFT = 0
+    STATUS_READY = 1
+    STATUS_WAITING_FOR_ANSWER = 2
+    STATUS_CHOICES = (
+        (STATUS_DRAFT, 'draft'),
+        (STATUS_READY, 'ready'),
+        (STATUS_WAITING_FOR_ANSWER, 'waiting_for_the_answer'),
+    )
+
+    state = FSMIntegerField(default=STATUS_DRAFT, choices=STATUS_CHOICES)
+
+    @transition(field=state, source=STATUS_DRAFT, target=STATUS_READY)
+    def publish(self):
+        pass
+
+    @transition(field=state, source=STATUS_READY, target=STATUS_WAITING_FOR_ANSWER)
+    def send(self):
+        pass
